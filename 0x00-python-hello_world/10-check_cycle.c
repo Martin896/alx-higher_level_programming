c4b6e659c5037523ec192635d809017e386cb005
Martin Okem Odachi <odachi054@gmail.com>
2024-03-11 20:30:43 -0700

    added a new file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: pointer to the head of the list
+ * Return: 1 if there is a cycle, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	tortoise = list;
+	hare = list->next;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		if (tortoise == hare)
+			return (1); /* Cycle detected */
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	return (0); /* No cycle found */
+}
+
