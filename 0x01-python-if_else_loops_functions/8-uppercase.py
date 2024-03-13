aaa596c4d8d1294e5475bc26ee1c6a443a7e6468
Martin Okem Odachi <odachi054@gmail.com>
2024-03-12 18:54:52 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-def print_last_digit(number):
-    last_digit = number % 10
-
-    print(last_digit)
-    return last_digit
-
-
-result = print_last_digit(12345)
-print("Value of the last digit:", result)
+def uppercase(some_str):
+    for char in some_str:
+        # Check if the character is a lowercase letter
+        if 'a' <= char <= 'z':
+            # Convert the lowercase letter to uppercase using ASCII values
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()  # Add a new line at the end
