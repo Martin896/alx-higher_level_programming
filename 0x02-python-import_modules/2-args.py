f87b2f907a14766768829abce77a531ee6c40bb6
Martin Okem Odachi <odachi054@gmail.com>
2024-04-20 12:20:20 -0700

    pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(num_arg))
-    for i in range (1,len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         print("{}:{}".format(i, sys.argv[i]))
